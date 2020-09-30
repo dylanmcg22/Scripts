c25f1452a0f6d4ce7faf73f03cbe4d02a3a8c673
dylanmcg22 <65293144+dylanmcg22@users.noreply.github.com>
2020-09-30 16:06:37 +0100

    Add files via upload

---
+++ inputaskquestions.py
@@ -0,0 +1,14 @@
+
+#basic excercise in asking for information
+
+print('Enter your name:')
+x = input()
+print('Hello, ' + x)
+
+print("What age are you? ")
+y = input()
+print("You are very young, I didn't think you were that young!" , y)
+
+z = input("Where are you from? " )
+print(f"I love that place {z}, and I went to school there!!")
+
